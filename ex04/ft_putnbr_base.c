3c6dd3c78398858a925569875792c35b6216b27b
Sueui Kang <suekang@c3r1s3.42seoul.kr>
2021-09-26 01:23:27 +0900

    C04

---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,59 @@
+#include <unistd.h>
+
+int	check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (base[0] == '\0' || base[1] == '\0')
+		return (0);
+	while (base[i])
+	{
+		j = i + 1;
+		if (base[i] == '+' || base[i] == '-' || base[i] < 32 || base[i] > 126)
+			return (0);
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	size;
+	int	nbr_last[100];
+	int	i;
+
+	i = 0;
+	size = 0;
+	if (check_base(base))
+	{
+		if (nbr < 0)
+		{
+			nbr = -nbr;
+			ft_putchar('-');
+		}
+		while (base[size] != '\0')
+			size++;
+		while (nbr > 0)
+		{
+			nbr_last[i] = base[nbr % size];
+			nbr /= size;
+			i++;
+		}
+		while (--i >= 0)
+			ft_putchar(nbr_last[i]);
+	}
+}
