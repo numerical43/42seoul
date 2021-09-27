fbd20847a094ab1b1d7489252193057d811a6a23
Sueui Kang <suekang@c1r2s6.42seoul.kr>
2021-09-27 23:40:37 +0900

    C06

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -3,43 +3,55 @@
 void	ft_putstr(char *str)
 {
 	while(*str)
-	{
 		write(1, str++, 1);
-	}
 }
-int	ft_strcmp(char *a, char *b)
+
+int	ft_strcmp(char *s1,char *s2)
 {
-	int	i;
+	while (*s1 != '\0' && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char*)s1 - *(unsigned char*)s2);
+}
+
+void	ft_str_sort(char **argv, int size)
+{
+	int		i;
+	int		j;
+	char	*temp;
 
 	i = 0;
-	while((a[i] == b[i]) && (a[i] != '\0' && b[i] != '\0'))
+	while (i < size)
+	{
+		j = 1;
+		while (j < size - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{	
+				temp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = temp;
+			}
+			j++;
+		}
 		i++;
-	return (a[i] - b[i]);
+	}
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
-	char *temp;
 
 	i = 1;
-	if (argv > 2)
-	{
-		while (i < argc - 1)
-		{				
-			if (argv[i] > argv[i + 1])
-			{
-				temp = argv[i];
-				argv[i] = argv[i + 1];
-				argv[i + 1] = temp;
-			}
-		}
-	}
-	i = 1;
+	if (argc > 2)
+		ft_str_sort(argv, argc - 1);
 	while (i < argc)
 	{
 		ft_putstr(argv[i]);
 		ft_putstr("\n");
+		i++;
 	}
 	return (0);
 
