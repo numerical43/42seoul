070a4a04799f547741dee1e07dfd1cfb53e42ab4
suekang <suekang@student.42seoul.kr>
2021-11-29 15:05:07 +0900

    modified

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -1,6 +1,16 @@
 #include "libft.h"
 
-int	ft_isalpha(int num)
+int	ft_isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
+}
+
+int	ft_islower(int c)
+{
+	return ((c >= 'a') && (c <= 'z'));
+}
+
+int	ft_isalpha(int c)
 {
 	return (ft_isupper(c) || ft_islower(c));
 }
--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -2,7 +2,7 @@
 
 int	ft_isprint(int c)
 {
-	if (c >= ' ' && c<= '~')
+	if (c >= ' ' && c <= '~')
 		return (1);
 	return (0);
 }
