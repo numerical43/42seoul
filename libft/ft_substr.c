4990b9d0804b12b80b47f05b13da5d2147057489
suekang <suekang@student.42seoul.kr>
2021-11-29 18:54:34 +0900

    non

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,1 +1,18 @@
 #include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*c;
+	int	i;
+
+	c = (char *)malloc(sizeof(char) * (len + 1));
+	if (c == NULL)
+		return (NULL);
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	if((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	i  = 
+	
+}
