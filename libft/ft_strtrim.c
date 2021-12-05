7df1bea54f038c62b2fa8aa9dfc29834e3a69d7f
suekang <suekang@student.42seoul.kr>
2021-12-05 16:26:17 +0900

    non

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,6 +1,18 @@
 #include "libft.h"
 
-char	*ft_strstrim(const char *s, unsigned int start, size_t len)
+char	*ft_strstrim(char const *s1, char const *set)
 {
-	
+	size_t	first;
+	size_t	last;
+	char	*c;
+
+	first = 0;
+	last = ft_strlen(s1) - 1;
+	c = (char *)malloc(sizeof(char *));
+	if (!s1)
+		return (NULL);
+	while (s1[start] && (s1[start] == set[start]))
+		start++;
+	while (s1[last] && (s1[last] == set[
+
 }
