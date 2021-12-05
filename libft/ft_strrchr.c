136c7dd9ef2ebb2b94fc51f9b375dea6d18c47f4
suekang <suekang@student.42seoul.kr>
2021-12-05 16:25:43 +0900

    modified

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,1 +1,17 @@
 #include "libft.h"
+
+char	*strrchr(const char *s, int c)
+{
+	char	find;
+	size_t	i;
+
+	i = ft_strlen(s) - 1;
+	find = (size_t)c;
+	while (i >= 0)
+	{
+		if (s[i] == find)
+		       return ((char *)s[i]);
+		i--;
+	}
+	return (NULL);
+}
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,1 +1,6 @@
 #include "libft.h"
+
+char	*ft_strstrim(const char *s, unsigned int start, size_t len)
+{
+	
+}
