d7a550d7c4258cc4698e9d9db0ac67fcfce14bd1
numerical43 <61495627+numerical43@users.noreply.github.com>
2021-12-28 10:41:50 +0900

    Update ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -19,9 +19,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i <= n)
 	{
-		if (*(unsigned char *)s == (unsigned char)c)
+		if ((unsigned char)s[i] == (unsigned char)c)
 			return ((void *)s);
-		s++;
 		i++;
 	}
 	return (0);
