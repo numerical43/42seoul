47e7c36a23c2fc8b914574096dd2007e86c01d38
numerical43 <61495627+numerical43@users.noreply.github.com>
2021-12-28 10:39:12 +0900

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -39,7 +39,7 @@ int	ft_atoi(const char *str)
 			sign = -1;
 		i++;
 	}
-	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
 	{
 		num *= 10;
 		num += str[i] - '0';
