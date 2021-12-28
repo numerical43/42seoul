e0458f5d6f7a47736373aa1ed3ba84abfabf8d2a
numerical43 <61495627+numerical43@users.noreply.github.com>
2021-12-28 10:33:50 +0900

    Update ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -19,7 +19,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	result;
 
 	i = 0;
-	dest_len = ft_strlen(dest);
+	destlen = ft_strlen(dest);
 	while (i < size - 1)
 	{
 		dest[i + destlen] = src[i];
