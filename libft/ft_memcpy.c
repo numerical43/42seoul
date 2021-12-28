6e5c6b38042d355b16909afe190e7fd974c20b71
numerical43 <61495627+numerical43@users.noreply.github.com>
2021-12-28 22:19:10 +0900

    Update ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -15,17 +15,17 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	unsigned char	*destination;
-	unsigned char	*source;
+	unsigned char	*d;
+	unsigned char	*s;
 
 	i = 0;
 	if (!dest || !src)
 		return (NULL);
-	destination = (unsigned char*)dest;
-	source = (unsigned char*)src;
+	d = (unsigned char*)dest;
+	s = (unsigned char*)src;
 	while (i < n)
 	{
-		destination[i] = source[i];
+		d[i] = s[i];
 		i++;
 	}
 	return (dest);
