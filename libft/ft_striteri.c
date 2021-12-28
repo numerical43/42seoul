a031d2adcf59089165d019052e88999060a048b5
numerical43 <61495627+numerical43@users.noreply.github.com>
2021-12-28 10:47:53 +0900

    Update ft_striteri.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -17,12 +17,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	int	i;
 
 	i = 0;
-	if (s && f)
-	{
-		while (s[i])
-		{
-			f(i, &s[i]);
-			i++;
-		}
-	}
+	while (s[i++])
+		f(i, &s[i]);
 }
