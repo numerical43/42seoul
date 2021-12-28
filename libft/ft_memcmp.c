da6b7b71c616bab977fc864310672ab4ba0adaa4
numerical43 <61495627+numerical43@users.noreply.github.com>
2021-12-28 11:54:58 +0900

    Update ft_memcmp.c

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -22,7 +22,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	if (!s1 || !s2)
-		return (NULL);
+		return (0);
 	while (i < n)
 	{
 		if (str1[i] != str2[i])
