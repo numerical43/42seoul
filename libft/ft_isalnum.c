833829c854159076497bb2bfe58b19aba7785ff4
numerical43 <61495627+numerical43@users.noreply.github.com>
2021-12-29 17:04:31 +0900

    Update ft_isalnum.c

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -12,6 +12,21 @@
 
 #include "libft.h"
 
+int	ft_isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
+}
+
+int	ft_islower(int c)
+{
+	return ((c >= 'a') && (c <= 'z'));
+}
+
+int	ft_isalpha(int c)
+{
+	return (ft_isupper(c) || ft_islower(c));
+}
+
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
