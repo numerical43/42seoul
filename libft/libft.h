13f9f0145968118da53091bdd1dc775ccc323eb2
numerical43 <61495627+numerical43@users.noreply.github.com>
2022-01-14 16:17:24 +0900

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -45,7 +45,7 @@ char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char    *ft_strnstr(const char *big, const char *little, size_t len);
 char    *ft_strrchr(const char *s, int c);
 char    *ft_strstrim(char const *s1, char const *set);
-char    *ft_strup(char *src, int size);
+char    *ft_strdup(char *src, int size);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 int     ft_tolower(int c);
 int     ft_toupper(int c);
