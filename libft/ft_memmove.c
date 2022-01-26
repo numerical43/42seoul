bd277da890cef56b8ae71532d679ddf9074ad17d
Sueui Kang <suekang@c6r9s6.42seoul.kr>
2022-01-26 21:59:27 +0900

    l

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: suekang <suekang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 20:11:30 by suekang           #+#    #+#             */
-/*   Updated: 2022/01/15 13:58:58 by suekang          ###   ########.fr       */
+/*   Updated: 2022/01/26 21:48:00 by suekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,26 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
 
 	i = 0;
 	if (dest == src)
 		return (dest);
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
 	if (dest < src)
 	{
 		while (i < n)
 		{
-			((char *)dest)[i] = ((char *)src)[i];
+			d[i] = s[i];
 			i++;
 		}
 	}
 	else
 	{
-		while (i < n)
-		{
-			((char *)dest)[n - i - 1] = ((char *)src)[n - i - 1];
-			i++;
-		}
+		while (n-- > 0)
+			d[n] = s[n];
 	}
 	return (dest);
 }
