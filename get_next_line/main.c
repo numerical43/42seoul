a3758a671b390d1f9e7a48197ce1af67b1ddef1f
numerical43 <61495627+numerical43@users.noreply.github.com>
2022-03-16 11:05:49 +0900

    Update main.c

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,12 +1,26 @@
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int   main(void)
 {
-	int     fd;
+	int     fd1, fd2, fd3, fd4, fd5;
 	char    *result;
 
-	fd = open("test.txt", O_RDONLY);
-	result = get_next_line(fd);
+	fd1 = open("test1.txt", O_RDONLY);
+	fd2 = open("test1.txt", O_RDONLY);
+	fd3 = open("test1.txt", O_RDONLY);
+	fd4 = open("test1.txt", O_RDONLY);
+	fd5 = open("test1.txt", O_RDONLY);
+	result = get_next_line(fd1);
+	printf("Hello\nWorld : %s\n", result);
+	result = get_next_line(fd2);
+	printf("\nWorld : %s\n", result);
+	result = get_next_line(fd3);
+	printf("empty : %s\n", result);
+	result = get_next_line(fd4);
+	printf("Hello\nWorld\n : %s\n", result);
+	result = get_next_line(fd5);
+	printf("\n : %s\n", result);
 	return (0);
 }
