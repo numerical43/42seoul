8e5d61d6327f17bbbedd312c934c11c5a65db91c
numerical43 <61495627+numerical43@users.noreply.github.com>
2022-04-11 22:36:01 +0900

    Create utils_not_num.c

---
+++ ft_printf/utils_not_num.c
@@ -0,0 +1,37 @@
+void	ifcharacter(s_list *ap)
+{
+	char result;
+	
+	result = va_arg(ap->arg, int);
+	ft_putchar_fd(result, 1);
+	ap->count++;	
+}
+
+void	ifstring(s_list *ap)
+{
+	char	*result;
+	
+	result = va_arg(ap->arg, char *);
+	if (!result)
+	{
+		ft_putstr_fd("(null)", 1);
+		ap->count++;
+	}
+	else
+	{
+		strlen = ft_strlen(result);
+		ap->count = ap->count + ft_strlen(result);;
+	}
+}
+
+void ifelsechar(s_list *ap, const char format)
+{
+	ft_putchar_fd(format, 1);
+	ap->count++;
+}
+
+void ifpercent(s_list *ap);
+{
+	ft_putchar_fd('%', 1);
+	ap->count++;
+}
